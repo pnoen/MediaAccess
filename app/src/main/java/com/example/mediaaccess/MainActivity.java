7cdfa128ea55164eabc12c25fd9489c05bcb3698
rton8097 <rton8097@uni.sydney.edu.au>
2023-09-15 22:17:25 +1000

    tutorial code

---
+++ app/src/main/java/com/example/mediaaccess/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.mediaaccess;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+    public final String APP_TAG = "MediaRecording";
+    public String photoFileName = "photo.jpg";
+    public String videoFileName = "video.mp4";
+    public String audioFileName = "audio.3gp";
+    //request codes
+    private static final int MY_PERMISSIONS_REQUEST_OPEN_CAMERA = 101;
+    private static final int MY_PERMISSIONS_REQUEST_READ_PHOTOS = 102;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_VIDEO = 103;
+    private static final int MY_PERMISSIONS_REQUEST_READ_VIDEOS = 104;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 105;
+
+    MarshmallowPermission marshmallowPermission = new MarshmallowPermission(this);
+
+    private File file;
+
+    // Returns the Uri for a photo/media stored on disk given the fileName and type
+    public Uri getFileUri(String fileName, int type) {
+        Uri fileUri = null;
+        try {
+            String typestr = "images"; //default to images type
+            if (type == 1) {
+                typestr = "videos";
+            } else if (type != 0) {
+                typestr = "audios";
+            }
+            // Get safe media storage directory depending on type
+            File mediaStorageDir = new File(getExternalFilesDir(Environment.getExternalStorageDirectory().toString()), APP_TAG);
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists()) {
+                mediaStorageDir.mkdirs();
+            }
+            // Create the file target for the media based on filename
+            file = new File(mediaStorageDir, fileName);
+            // Wrap File object into a content provider, required for API >= 24
+            // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+            if (Build.VERSION.SDK_INT >= 24) {
+                fileUri = FileProvider.getUriForFile(
+                        this.getApplicationContext(),
+                        "com.example.mediaaccess.fileProvider", file);
+            } else {
+                fileUri = Uri.fromFile(mediaStorageDir);
+            }
+        } catch (Exception ex) {
+            Log.d("getFileUri", ex.getStackTrace().toString());
+        }
+        return fileUri;
+    }
+
+    public void onTakePhotoClick(View v) {
+        // Check permissions
+        if (!marshmallowPermission.checkPermissionForCamera()) {
+            marshmallowPermission.requestPermissionForCamera();
+        } else {
+            // create Intent to take a picture and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // set file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+            photoFileName = "IMG_" + timeStamp + ".jpg";
+
+            // Create a photo file reference
+            Uri file_uri = getFileUri(photoFileName, 0);
+
+            // Add extended data to the intent
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file_uri);
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, MY_PERMISSIONS_REQUEST_OPEN_CAMERA);
+            }
+        }
+    }
+
+    public void onLoadPhotoClick(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // Bring up gallery to select a photo
+        startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_PHOTOS);
+
+    }
+
+    public void onLoadVideoClick(View view) {
+        // Create intent for picking a video from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+
+        // Bring up gallery to select a video
+        startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_VIDEOS);
+    }
+
+    public void onRecordVideoClick(View v) {
+        // Check permissions
+        if (!marshmallowPermission.checkPermissionForCamera()) {
+            marshmallowPermission.requestPermissionForCamera();
+        } else {
+            // create Intent to capture a video and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            // set file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+            videoFileName = "VIDEO_" + timeStamp + ".mp4";
+
+            // Create a video file reference
+            Uri file_uri = getFileUri(videoFileName, 1);
+
+            // add extended data to the intent
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file_uri);
+
+            // Start the video record intent to capture video
+            startActivityForResult(intent, MY_PERMISSIONS_REQUEST_RECORD_VIDEO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final VideoView mVideoView = (VideoView) findViewById(R.id.videoView);
+        ImageView ivPreview = (ImageView) findViewById(R.id.imageView);
+        mVideoView.setVisibility(View.GONE);
+        ivPreview.setVisibility(View.GONE);
+        if (requestCode == MY_PERMISSIONS_REQUEST_OPEN_CAMERA) {
+            Log.i("Code", String.valueOf(resultCode));
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+                // Load the taken image into a preview
+                ivPreview.setImageBitmap(takenImage);
+                ivPreview.setVisibility(View.VISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken AAA!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_READ_PHOTOS) {
+            if (resultCode == RESULT_OK) {
+                Uri photoUri = data.getData();
+                Bitmap selectedImage;
+                try {
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+                    ivPreview.setImageBitmap(selectedImage);
+                    ivPreview.setVisibility(View.VISIBLE);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_READ_VIDEOS) {
+            if (resultCode == RESULT_OK) {
+                Uri videoUri = data.getData();
+                mVideoView.setVisibility(View.VISIBLE);
+                mVideoView.setVideoURI(videoUri);
+                mVideoView.requestFocus();
+                mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    // Close the progress bar and play the video
+                    public void onPrepared(MediaPlayer mp) {
+                        mVideoView.start();
+                    }
+                });
+            }
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_RECORD_VIDEO) {
+            //if you are running on emulator remove the if statement
+//            if (resultCode == RESULT_OK) {
+//
+//            }
+            Uri takenVideoUri = getFileUri(videoFileName, 1);
+            mVideoView.setVisibility(View.VISIBLE);
+            mVideoView.setVideoURI(takenVideoUri);
+            mVideoView.requestFocus();
+            mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                // Close the progress bar and play the video
+                public void onPrepared(MediaPlayer mp) {
+                    mVideoView.start();
+                }
+            });
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
